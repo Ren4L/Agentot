49bd1178033cf5797553d47c503a3fd3de21c558
Ren4L <vladisakov28@gmail.com>
2022-07-15 15:10:17 +0300

    fix bugs

--- src/Home/NavBarAdaptive.js
+++ src/Home/NavBarAdaptive.js
@@ -3,20 +3,23 @@ import '../Static/Static.css';
 import { Link } from 'react-scroll';
 
 export default function NavBarAdaptive(){
+    function Close(){
+        document.querySelector('.navContAdaptive').style.right = '-100vw';
+    }
     return(
         <nav>
             <img src={require('../Static/icon.png')} alt='Logo'/>
             <img src={require('./Open.png')} alt='Open' style={{width:'30px', cursor:'pointer'}} onClick={() => document.querySelector('.navContAdaptive').style.right = '0vw'}/>
             <div className='navContAdaptive'>
                 <div style={{width:'100vw', display:'flex', justifyContent:'right', paddingRight:'10vw', cursor:'pointer'}}>
-                    <img src={require('./Close.png')} alt='Close' style={{width:'30px'}} onClick={() => document.querySelector('.navContAdaptive').style.right = '-100vw'}/>
+                    <img src={require('./Close.png')} alt='Close' style={{width:'30px'}} onClick={Close}/>
                 </div>
-                <Link to='Header' smooth={true} duration={500}>Главная</Link>
-                <Link to='Services' smooth={true} duration={500}>Услуги</Link>
-                <Link to='About' smooth={true} duration={500}>О нас</Link>
-                <Link to='Permissions' smooth={true} duration={500}>Разрешения</Link>
-                <Link to='Partners' smooth={true} duration={500}>Наши клиенты</Link>
-                <Link to='Contact' smooth={true} duration={500}>Контакты</Link>
+                <Link to='Header' smooth={true} duration={500} onClick={Close}>Главная</Link>
+                <Link to='Services' smooth={true} duration={500} onClick={Close}>Услуги</Link>
+                <Link to='About' smooth={true} duration={500} onClick={Close}>О нас</Link>
+                <Link to='Permissions' smooth={true} duration={500} onClick={Close}>Разрешения</Link>
+                <Link to='Partners' smooth={true} duration={500} onClick={Close}>Наши клиенты</Link>
+                <Link to='Contact' smooth={true} duration={500} onClick={Close}>Контакты</Link>
             </div>
         </nav>
     )
--- src/ServicesTemplate/ServicesTemplate.js
+++ src/ServicesTemplate/ServicesTemplate.js
@@ -6,8 +6,10 @@ import { Link } from 'react-router-dom';
 export default function ServicesTemplate(props){
     return(
         <div className='ServicesTemplateCont'>
-            <Link to='/' className='header'>На главную</Link>
-            <img src={require('../Static/icon.png')} alt='logo' className='Logo'/>
+            <div style={{display:'flex', flexDirection:'row', alignItems:'center', justifyContent:"left", width:'100vw'}}>
+                <Link to='/' className='header'>На главную</Link>
+                <img src={require('../Static/icon.png')} alt='logo' className='Logo'/>
+            </div>
             <div className='ServicesTemplateName'>
                 <img src={require('../Static/left.png')} alt='Left' style={{marginRight:'0%', width:'25vw'}}/>
                 <div style={{width:'50vw'}}>{props.title}</div>
